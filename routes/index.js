b7541ca8be7b31c36cf792d59b7d7c9ccd0ee7fa
mahyudin akbar <mahyudinakbar10@gmail.com>
2023-05-12 10:36:07 +0700

    view done

---
+++ routes/index.js
@@ -0,0 +1,11 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+router.get('/add', (req,res,next)=>{
+  res.render('add', {title : 'Add'})
+});
+module.exports = router;
