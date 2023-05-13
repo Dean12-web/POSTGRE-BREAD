80fde017f2653bf4aaeb18e72a0e1345b80dac9c
mahyudin akbar <mahyudinakbar10@gmail.com>
2023-05-13 18:34:59 +0700

    Pagination without limit or req.url

--- routes/index.js
+++ routes/index.js
@@ -4,62 +4,62 @@ const moment = require('moment')
 
 module.exports = (pool) => {
   /* GET home page. */
-
-  // pool.query('SELECT * FROM data', (err, res) => {
-  //   if (err) {
-  //     console.error(err)
-  //   } else {
-  //     console.log(res.rows)
-  //   }
-  // })
-  // pool.end()
   router.get('/', function (req, res, next) {
-    const currentDate = `SELECT date FROM data WHERE id = 1`
-    const sql = `SELECT * FROM data`
-    pool.query(sql,(err,row)=>{
-      if(err){
-        console.error(err)
-      }else{
-        // console.log(row.rows[0].date)
-        const date = row.rows[0].date
-        res.render('index', { title: 'POSTGRE-Breads',data : row.rows, moment});
-      }
-    })
+    // pagination
+    const sqlCount = `SELECT COUNT(*) as total FROM data`
+    pool.query(sqlCount, (err, row) => {
+      const rows = row.rows[0].total
+      // console.log(rows)
+      const page = req.query.page || 1
+      const limit = 3
+      const offset = (page - 1) * limit
+      const pages = Math.ceil(rows/limit)
+      const sql = `SELECT * FROM data ORDER BY id ASC LIMIT ${limit} OFFSET ${offset} `
+      pool.query(sql, (err, row) => {
+        if (err) {
+          console.error(err)
+        } else {
+          // console.log(row.rows[0].date)
+          const date = row.rows[0].date
+          res.render('index', { title: 'POSTGRE-Breads', data: row.rows, moment, page : page, pages:pages});
+        }
+      })
+    });
   });
   router.get('/add', (req, res, next) => {
     res.render('add', { title: 'Add' })
   });
 
-  router.post('/add', (req,res,next)=>{
+  router.post('/add', (req, res, next) => {
     const string = req.body.string
     const integer = req.body.integer
     const float = req.body.float
     const date = req.body.date
     const boolean = req.body.boolean
     const sql = `INSERT INTO data(string,integer,float,date,boolean) VALUES('${string}', ${integer}, ${float},'${date}',${boolean})`
-    pool.query(sql,(err)=>{
-      if(err){
+    pool.query(sql, (err) => {
+      if (err) {
         console.error(err)
-      }else{
+      } else {
         console.log('ADDING DATA SUCCESS')
         res.redirect('/')
       }
     });
   });
 
-  router.get('/edit/:id',(req,res,next)=>{
+  router.get('/edit/:id', (req, res, next) => {
     const id = req.params.id
     const sql = `SELECT * FROM data WHERE id = ${id}`
-    pool.query(sql,(err,row)=>{
-      if(err){
+    pool.query(sql, (err, row) => {
+      if (err) {
         console.error(err)
-      }else{
-        res.render('edit', {title : 'Edit', data : row.rows[0],moment})
+      } else {
+        res.render('edit', { title: 'Edit', data: row.rows[0], moment })
       }
     });
   })
 
-  router.post('/edit/:id',(req,res,next)=>{
+  router.post('/edit/:id', (req, res, next) => {
     const id = req.params.id
     const string = req.body.string
     const integer = req.body.integer
@@ -67,11 +67,24 @@ module.exports = (pool) => {
     const date = req.body.date
     const boolean = req.body.boolean
     let sql = `UPDATE data SET string= '${string}', integer = ${integer}, float = ${float},date = '${date}', boolean = ${boolean} WHERE id = ${id}`
-    pool.query(sql,(err)=>{
-      if(err){
+    pool.query(sql, (err) => {
+      if (err) {
         console.error(err)
-      }else{
+      } else {
         console.log('EDIT DATA SUCCESS')
+        res.redirect('/')
+      }
+    })
+  })
+
+  router.get('/delete/:id', (req, res, next) => {
+    const id = req.params.id
+    const sql = `DELETE FROM data WHERE id = ${id}`
+    pool.query(sql, (err) => {
+      if (err) {
+        console.error(err)
+      } else {
+        console.log('DELETE DATA SUCCESS')
         res.redirect('/')
       }
     })
